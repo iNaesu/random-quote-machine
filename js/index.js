76223834643da408daa0026ccf1a6c035a88aef9
Sean Law <sean.law.321@gmail.com>
2017-08-04 23:27:19 +1000

    Restructure project

--- js/index.js
+++ js/index.js
@@ -1,29 +1,79 @@
-var idx = 0;
+/* Globals ------------------------------------------------------------------ */
 
-/* Render new quote on button click. */
-$("#new-quote-button").click(function() {
-  /* Get and render quote. */
-  $.getJSON( "https://favqs.com/api/qotd", function(data) {
-    $("#quote").fadeOut(250, function() {
-      $("#quote").text(data.quote.body).fadeIn(250);
-    });
-    $("#author").fadeOut(250, function() {
-      $("#author").text(data.quote.author).fadeIn(250);
-    });
-    
-    /* Change bg color.  */
-    var bgColors = ["#FCD1CC", "#AEE0F9", "#FFF2B5", "#C3C5E6", "#D1FFBF", "#AEE0F9", "#FCD1CC", "#A8CBD1", "#F7C6D3"];
-    if (++idx >= bgColors.length) {
-      idx = 0;
-    }
-    $("#background").css("background-color", bgColors[idx]);
+let idx = 0;
+
+
+/* Function Declarations ---------------------------------------------------- */
+
+/**
+ * Get quote from FavQs API
+ * URL: https://favqs.com
+ * @param {function} callback
+ */
+function getQuote(callback) {
+  $.ajax({
+    url: 'https://favqs.com/api/qotd',
+    timeout: 5000,
+    success: function(quoteData) {
+      callback(quoteData.quote);
+    },
+    error: function(error) {
+      /* Fall back to a hardcoded quote */
+      const quote = {
+        'body': 'Be yourself; everyone else is taken.',
+        'author': 'Oscar Wilde',
+      };
+      callback(quote);
+    },
   });
-});
+}
 
-/* Tweet quote on Twitter bird click. */
-$("#twitter-bird").click(function() {
-  var quote = $("#quote").text();
-  var author = $("#author").text();
-  
-  $(this).attr("href", `https://twitter.com/intent/tweet?text=\"${quote}\" ${author}`);
+/**
+ * Display app
+ * @param {Object} quoteData
+ * @param {string} quoteData.body
+ * @param {string} quoteData.author
+ */
+function displayApp(quoteData) {
+  const quote = $('#quote');
+  const author = $('#author');
+
+  /* Quote */
+  quote.fadeOut(250, function() {
+    quote.text(quoteData.body).fadeIn(250);
+  });
+
+  /* Author */
+  author.fadeOut(250, function() {
+    author.text(quoteData.author).fadeIn(250);
+  });
+
+  /* Change bg color  */
+  const bgColors = [
+    '#FCD1CC', '#AEE0F9', '#FFF2B5', '#C3C5E6', '#D1FFBF', '#AEE0F9', '#FCD1CC',
+    '#A8CBD1', '#F7C6D3',
+  ];
+  if (++idx >= bgColors.length) {
+    idx = 0;
+  }
+  $('body').css('background-color', bgColors[idx]);
+}
+
+
+/* Start of Script ---------------------------------------------------------- */
+
+$(document).ready(function() {
+  $('#new-quote-button').click(function() {
+    /* Get quote & refresh the app */
+    getQuote(displayApp);
+  });
+
+  /* Tweet quote on Twitter bird click. */
+  $('#twitter-bird').click(function() {
+    const quote = $('#quote').text();
+    const author = $('#author').text();
+    $(this).attr(
+      'href', `https://twitter.com/intent/tweet?text='${quote}' ${author}`
+    );
+  });
 });
